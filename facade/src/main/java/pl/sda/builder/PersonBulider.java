f280edfcfc9207e9072d32e89465727ee7cf364e
unknown <ggozdziak@gmail.com>
2017-02-25 11:46:55 +0100

    Initial commit

---
+++ facade/src/main/java/pl/sda/builder/PersonBulider.java
@@ -0,0 +1,33 @@
+package pl.sda.builder;
+
+public class PersonBulider {
+    private Person person;
+
+    public PersonBulider() {
+        person = new Person();
+    }
+
+    public PersonBulider withFirstName(String firstName) {
+        person.setFirstName(firstName);
+        return this;
+    }
+
+    public PersonBulider withLastName(String lastName) {
+        person.setLastName(lastName);
+        return this;
+    }
+
+    public PersonBulider withPersonAddress(PersonAddress personAddress) {
+        person.setAddress(personAddress);
+        return this;
+    }
+
+    public PersonBulider withPesel(String pesel) {
+        person.setPesel(pesel);
+        return this;
+    }
+
+    public Person build() {
+        return person;
+    }
+}
