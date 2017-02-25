c6d1632223936d2164ac8b6097368aa928ca3e12
unknown <ggozdziak@gmail.com>
2017-02-25 11:52:43 +0100

    Added some design patterns examples: facade and builder

---
+++ designPatterns/src/main/java/pl/sda/facade/main.java
@@ -0,0 +1,17 @@
+package pl.sda.facade;
+
+import pl.sda.sort.Sort;
+import pl.sda.sort.impl.InsertSort;
+
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] args) {
+
+
+        int[] tab = new int[] {553, 55, 1, 67, 1, 77, 0};
+        SortFacade sort = new SortFacade();
+        int[] result = sort.sortThroughFacade(tab);
+        System.out.println(Arrays.toString(result));
+    }
+}
